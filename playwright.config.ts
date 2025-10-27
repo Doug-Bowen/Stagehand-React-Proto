39b6af9b4c81efc49a79e45ea55b0bcda42025d5
doug-bowen <douglastylerbowen@gmail.com>
2025-10-27 15:50:41 -0500

    add pw config

---
+++ playwright.config.ts
@@ -0,0 +1,44 @@
+import { PlaywrightTestConfig } from '@playwright/test';
+
+// Consumes environment variables for tests to utilize
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({ path: path.resolve(__dirname, './', '.env') });
+dotenv.config({ path: path.resolve(__dirname, './', '.env.development') });
+
+const config: PlaywrightTestConfig = {
+    testDir: 'E2E/Tests/',
+    fullyParallel: true,
+    timeout: 180 * 1000, //Global Test Cutoff
+    retries: 0,
+    reporter: [
+        ['list'],
+        ['html', { open: 'never' }]
+    ],
+    use: {
+        headless: false,
+        screenshot: 'only-on-failure',
+        trace: 'retain-on-failure',
+        ignoreHTTPSErrors: true,
+        viewport: { width: 1920, height: 1080 }
+    },
+    projects: [
+        {
+            name: 'Chrome',
+            use: { browserName: 'chromium', channel: 'chromium' }
+        },
+        {
+            name: 'Edge',
+            use: { browserName: 'chromium', channel: 'msedge' }
+        },
+        {
+            name: 'Safari',
+            use: { browserName: 'webkit' }
+        },
+        {
+            name: 'Firefox',
+            use: { browserName: 'firefox' }
+        }
+    ]
+};
+export default config;
